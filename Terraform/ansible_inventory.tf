fb912225fc8ce7510b7f0755d97a4511acc5b357
Abdelhamid108 <abdelhamidmido10@gmail.com>
2025-11-07 17:28:14 +0000

    modfiied terraform to enable passing needed anible inventory file dynamiclly , add playbook aand role to install needed packegs

---
+++ Terraform/ansible_inventory.tf
@@ -0,0 +1,12 @@
+# This resource generates the Ansible inventory file dynamically
+# after 'terraform apply' completes, using the outputs from the compute module.
+
+resource "local_file" "ansible_inventory" {
+  # This path goes up one level and into the Ansible inventory directory
+  filename = "../Ansible/inventory/hosts.ini"
+
+  content = templatefile("${path.module}/ansible_inventory.tpl", {
+    master_public_ip   = module.compute.master_public_ip
+    worker_private_ips = module.compute.worker_private_ips
+  })
+}
