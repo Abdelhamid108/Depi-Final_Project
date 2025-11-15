c470a84887d8d0390e761aa04ff102c17a226c5d
Abdelhamid108 <abdelhamidmido10@gmail.com>
2025-11-15 23:55:31 +0000

    configure cluster to enable using aws services

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -14,16 +14,28 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidr" {
+variable "public_subnet_cidr1" {
   description = "The CIDR block for the public subnet."
   type        = string
   default     = "10.0.1.0/24"
 }
 
-variable "private_app_subnet_cidr" {
-  description = "The CIDR block for the private application subnet."
+variable "public_subnet_cidr2" {
+  description = "The CIDR block for the public subnet."
   type        = string
   default     = "10.0.2.0/24"
+}
+
+variable "private_app_subnet_cidr1" {
+  description = "The CIDR block for the private application subnet."
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
+variable "private_app_subnet_cidr2" {
+  description = "The CIDR block for the private application subnet."
+  type        = string
+  default     = "10.0.4.0/24"
 }
 
 variable "master_instance_type" {
